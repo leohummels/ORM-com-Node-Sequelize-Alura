c203a80328468439e32105efc6a7cdadd618e968
leohummels <lsousa767@gmail.com>
2022-05-10 22:02:07 -0300

    CRUD completo

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -10,6 +10,49 @@ class PessoaController {
             return res.status(500).json(error.message)
         }
     }
+
+    static async pegaPessoa(req, res){
+        const {id} = req.params 
+        try {
+            const pessoa = await database.Pessoas.findOne({where: { id: Number(id) }})
+            return res.status(200).json(pessoa)
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async criaPessoa(req, res){
+        const novaPessoa = req.body
+        try {
+            const pessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(200).json(pessoaCriada)
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async atualizaPessoa(req, res) {
+        const novasInfos = req.body
+        const { id } = req.params
+        try {
+            await database.Pessoas.update(novasInfos, {where: { id: Number(id)}})
+            const pessoaAtualizada = await database.Pessoas.findOne({where: { id: Number(id)}})
+            return res.status(200).json(pessoaAtualizada)
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async apagaPessoa(req, res) {
+        const {id} = req.params
+        try {
+            await database.Pessoas.destroy({where: {id: Number(id)}})
+            return res.status(200).json({mensagem: `${id} deletado` })
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
+
 
 module.exports = PessoaController
